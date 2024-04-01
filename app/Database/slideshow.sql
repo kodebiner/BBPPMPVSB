204db58e926d25db34a6f3e986e3ba73af91611f
S3ME <99646777+S3ME@users.noreply.github.com>
2024-04-01 13:11:30 +0700

    Database Field Slideshow

---
+++ app/Database/slideshow.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 01, 2024 at 08:10 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bbppmpvsb`
+--
+
+--
+-- Dumping data for table `slideshow`
+--
+
+INSERT INTO `slideshow` (`id`, `file`, `status`) VALUES
+(1, 'wbbm website.png', 1),
+(2, 'wbbm website (1).png', 1),
+(3, 'wbbm website (5).png', 1),
+(4, 'wbbm website (10).png', 1),
+(5, 'wbbm website (11).png', 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
