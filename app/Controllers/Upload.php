4a8a87d932dcbb41a65289734e93bcc68b69af2d
ARBAATURRIZAL <63523333+ARBAATURRIZAL@users.noreply.github.com>
2024-04-26 19:39:20 +0700

    update controller

--- app/Controllers/Upload.php
+++ app/Controllers/Upload.php
@@ -343,108 +343,112 @@ class Upload extends BaseController
          ***************************************************/
         // $accepted_origins = array("http://localhost", "http://192.168.1.1", "http://127.0.0.1:8000", "http://127.0.0.1");
         // $accepted_origins = array("http:///bbppmpvsb.local", "http://192.168.1.1", "http://127.0.0.1:8000", "http://127.0.0.1");
-        $accepted_origins = array("http://bbppmpvsb.local",);
+        // $accepted_origins = array("http://bbppmpvsb.local",);
 
-        /*********************************************
-         * Change this line to set the upload folder *
-         *********************************************/
+        // /*********************************************
+        //  * Change this line to set the upload folder *
+        //  *********************************************/
 
-        $method = $_SERVER['REQUEST_METHOD'];
-        if ($method == 'OPTIONS') {
-            if (isset($_SERVER['HTTP_ORIGIN'])) {
-                if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
-                    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-                    header("HTTP/1.1 200 OK");
-                    return;
-                } else {
-                    header("HTTP/1.1 403 Origin Denied");
-                    return;
-                }
-            }
-        } elseif ($method == 'POST') {
-            $imageFolder = "/images/";
-            reset($_FILES);
-            $temp = current($_FILES);
-            if (is_uploaded_file($temp['tmp_name'])) {
-                header('CUS_MSG1: hello');
-                if (isset($_SERVER['HTTP_ORIGIN'])) {
-                    // same-origin requests won't set an origin. If the origin is set, it must be valid.
-                    if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
-                        header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-                    } else {
-                        header("HTTP/1.1 403 Origin Denied");
-                        return;
-                    }
-                }
+        // $method = $_SERVER['REQUEST_METHOD'];
+        // if ($method == 'OPTIONS') {
+        //     if (isset($_SERVER['HTTP_ORIGIN'])) {
+        //         if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
+        //             header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        //             header("HTTP/1.1 200 OK");
+        //             return;
+        //         } else {
+        //             header("HTTP/1.1 403 Origin Denied");
+        //             return;
+        //         }
+        //     }
+        // } elseif ($method == 'POST') {
+        //     $imageFolder = "/images/";
+        //     reset($_FILES);
+        //     $temp = current($_FILES);
+        //     if (is_uploaded_file($temp['tmp_name'])) {
+        //         header('CUS_MSG1: hello');
+        //         if (isset($_SERVER['HTTP_ORIGIN'])) {
+        //             // same-origin requests won't set an origin. If the origin is set, it must be valid.
+        //             if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
+        //                 header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        //             } else {
+        //                 header("HTTP/1.1 403 Origin Denied");
+        //                 return;
+        //             }
+        //         }
 
-                /*
-            If your script needs to receive cookies, set images_upload_credentials : true in
-            the configuration and enable the following two headers.
-            */
-                // header('Access-Control-Allow-Credentials: true');
-                // header('P3P: CP="There is no P3P policy."');
+        //         /*
+        //     If your script needs to receive cookies, set images_upload_credentials : true in
+        //     the configuration and enable the following two headers.
+        //     */
+        //         // header('Access-Control-Allow-Credentials: true');
+        //         // header('P3P: CP="There is no P3P policy."');
 
-                // Sanitize input
-                if (preg_match("/([^\w\s\d\-_~,;:\[\]\(\).])|([\.]{2,})/", $temp['name'])) {
-                    header("HTTP/1.1 400 Invalid file name.");
-                    return;
-                }
+        //         // Sanitize input
+        //         if (preg_match("/([^\w\s\d\-_~,;:\[\]\(\).])|([\.]{2,})/", $temp['name'])) {
+        //             header("HTTP/1.1 400 Invalid file name.");
+        //             return;
+        //         }
 
-                // Verify extension
-                if (!in_array(strtolower(pathinfo($temp['name'], PATHINFO_EXTENSION)), array("gif", "jpg", "png"))) {
-                    header("HTTP/1.1 400 Invalid extension.");
-                    return;
-                }
+        //         // Verify extension
+        //         if (!in_array(strtolower(pathinfo($temp['name'], PATHINFO_EXTENSION)), array("gif", "jpg", "png"))) {
+        //             header("HTTP/1.1 400 Invalid extension.");
+        //             return;
+        //         }
 
-                // Accept upload if there was no origin, or if it is an accepted origin
-                $filetowrite = $imageFolder . $temp['name'];
-                move_uploaded_file($temp['tmp_name'], $filetowrite);
+        //         // Accept upload if there was no origin, or if it is an accepted origin
+        //         $filetowrite = $imageFolder . $temp['name'];
+        //         move_uploaded_file($temp['tmp_name'], $filetowrite);
 
-                // Respond to the successful upload with JSON.
-                // Use a location key to specify the path to the saved image resource.
-                // { location : '/your/uploaded/image/file'}
-                echo json_encode(array('location' => 'http://' . $_SERVER['SERVER_NAME'] . $filetowrite));
-            } else {
-                // Notify editor that the upload failed
-                header("HTTP/1.1 500 Server Error");
-            }
-        } else {
-            // Notify editor that the upload failed
-            header("HTTP/1.1 500 Server Error");
-        }
+        //         // Respond to the successful upload with JSON.
+        //         // Use a location key to specify the path to the saved image resource.
+        //         // { location : '/your/uploaded/image/file'}
+        //         echo json_encode(array('location' => 'http://' . $_SERVER['SERVER_NAME'] . $filetowrite));
+        //         // echo json_encode(array('location' => 'http://bbppmpvsb.local' . $filetowrite));
+        //         // echo json_encode(array('location' => 'http://bbppmpvsb.local' . $filetowrite));
+        //         // return json_encode(array('location' => 'http://' . $_SERVER['SERVER_NAME'] . $filetowrite));
+        //         // return json_encode(array('location' => 'http://bbppmpvsb.local' . $filetowrite));
+        //     } else {
+        //         // Notify editor that the upload failed
+        //         header("HTTP/1.1 500 Server Error");
+        //     }
+        // } else {
+        //     // Notify editor that the upload failed
+        //     header("HTTP/1.1 500 Server Error");
+        // }
 
         // $url = array(
         //     "http://localhost"
         // );
 
-        // reset($_FILES);
-        // $temp = current($_FILES);
+        reset($_FILES);
+        $temp = current($_FILES);
 
-        // if (is_uploaded_file($temp['tmp_name'])) {
-        //     if (preg_match("/([^\w\s\d\-_~,;:\[\]\(\).])|([\.]{2,})/", $temp['name'])) {
-        //         header("HTTP/1.1 400 Invalid file name,Bad request");
-        //         return;
-        //     }
+        if (is_uploaded_file($temp['tmp_name'])) {
+            if (preg_match("/([^\w\s\d\-_~,;:\[\]\(\).])|([\.]{2,})/", $temp['name'])) {
+                header("HTTP/1.1 400 Invalid file name,Bad request");
+                return;
+            }
             
-        //     // Validating File extensions
-        //     if (! in_array(strtolower(pathinfo($temp['name'], PATHINFO_EXTENSION)), array(
-        //         "gif",
-        //         "jpg",
-        //         "png"
-        //     ))) {
-        //         header("HTTP/1.1 400 Not an Image");
-        //         return;
-        //     }
+            // Validating File extensions
+            if (! in_array(strtolower(pathinfo($temp['name'], PATHINFO_EXTENSION)), array(
+                "gif",
+                "jpg",
+                "png"
+            ))) {
+                header("HTTP/1.1 400 Not an Image");
+                return;
+            }
             
-        //     $fileName = $temp['name'];
-        //     move_uploaded_file($temp['tmp_name'], $fileName);
+            $fileName = "images/". $temp['name'];
+            move_uploaded_file($temp['tmp_name'], $fileName);
             
-        //     // Return JSON response with the uploaded file path.
-        //     echo json_encode(array(
-        //         'location' => 'http://bbppmpvsb.local/images/' . $fileName
-        //     ));
-        // 
-        // }
+            // Return JSON response with the uploaded file path.
+            echo json_encode(array(
+                'location' => 'http://bbppmpvsb.local/images/' . $fileName
+            ));
+        
+        }
     }
 
     public function removefotoberita()
@@ -807,6 +811,7 @@ class Upload extends BaseController
 
         // Get Data
         $input  = $this->request->getPost();
+        dd($input);
         $user   = $UserModel->find($this->data['uid']);
 
         // Validation Rules
